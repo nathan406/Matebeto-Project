1cfd7abcad2310cca60731d2d023d65d5efec78e
xililo <sililosililo82@gmail.com>
2023-01-02 11:07:49 +0200

    created base template

--- Base/urls.py
+++ Base/urls.py
@@ -1,8 +1,10 @@
 from Base import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("",views.index ,name="home"),
     path("authDirect/",views.authDirect, name="authDirect"),
     path("LoginRegister/",views.LoginRegisterUser , name="LoginRegisterUser")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
